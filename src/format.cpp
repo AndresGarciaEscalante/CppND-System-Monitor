a5a73a0931bd169bc432b45b78c6668d3aec56ad
AndresGarciaEscalante <andres_ge@hotmail.es>
2023-08-07 22:20:25 -0500

    fix: Fix the time format

--- src/format.cpp
+++ src/format.cpp
@@ -11,8 +11,8 @@ using std::to_string;
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) { 
     long int sec, minutes,hours;
-    hours=(int)(seconds/3600);
-    minutes=((int)(seconds/60))%60;
-    sec=(int)(seconds%60);
+    hours=seconds/3600;
+    minutes=(seconds%3600)/60;
+    sec=(seconds%3600)%60;
     return string(to_string(hours)+":" + to_string(minutes)+ ":"+ to_string(sec));
 }
